8e8bf1763940359e97f5d604211961d3da4e1972
林芊妍 <113121134@gms.tcu.edu.tw>
2026-05-20 16:04:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Mermaid-Horror/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
